e6d65797fe5c9f9dfd56dc98fbceac0558819216
Sandro Santilli <strk@kbt.io>
2022-01-15 16:30:32 +0100

    [dronie] give a summary of upgrade tests

--- ci/dronie/postgis_regress.sh
+++ ci/dronie/postgis_regress.sh
@@ -6,17 +6,21 @@ set -e
 service postgresql start $PGVER
 export PGPORT=`grep ^port /etc/postgresql/$PGVER/main/postgresql.conf | awk '{print $3}'`
 export PATH=/usr/lib/postgresql/$PGVER/bin:$PATH
-psql --version
+#psql --version
 ./autogen.sh
 ./configure CFLAGS="-O2 -Wall -fno-omit-frame-pointer -Werror" --without-interrupt-tests
 make clean
 make -j
 # we should maybe wait for postgresql service to startup here...
-psql -c "select version()" template1
+#psql -c "select version()" template1
 RUNTESTFLAGS=-v make check
 make install
 RUNTESTFLAGS=-v make installcheck
 
 CURRENTVERSION=`grep '^POSTGIS_' Version.config | cut -d= -f2 | paste -sd '.'`
-utils/check_all_upgrades.sh -s ${CURRENTVERSION}
-utils/check_all_upgrades.sh -s ${CURRENTVERSION}!
+utils/check_all_upgrades.sh -s ${CURRENTVERSION} | tee check.log
+echo "-- Summary of upgrade tests --"
+egrep '(PASS|FAIL)' check.log
+utils/check_all_upgrades.sh -s ${CURRENTVERSION}! | tee check.log
+echo "-- Summary of upgrade tests (using postgis_extensions_upgrade when available) --"
+egrep '(PASS|FAIL)' check.log
