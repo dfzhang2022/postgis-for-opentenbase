3607e0910b1d0cb5845e70e7030ef071f8577d42
Regina Obe <lr@pcorp.us>
2022-02-02 01:22:48 -0500

    More fixes

--- extensions/make_unpackaged.sql
+++ extensions/make_unpackaged.sql
@@ -1,6 +1,6 @@
 ﻿-- for postgis
-SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || regexp_replace(
-    regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
+SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || pg_catalog.regexp_replace(
+    pg_catalog.regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
     E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
 FROM pg_catalog.pg_depend AS d
 INNER JOIN pg_extension AS e ON (d.refobjid = e.oid)
@@ -9,8 +9,8 @@ AND deptype = 'e' AND e.extname = 'postgis'
 ORDER BY sqladd;
 
 -- for topology
-SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || regexp_replace(
-    regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
+SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || pg_catalog.regexp_replace(
+    pg_catalog.regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
     E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
 FROM pg_catalog.pg_depend AS d
 INNER JOIN pg_extension AS e ON (d.refobjid = e.oid)
@@ -19,8 +19,8 @@ AND deptype = 'e' AND e.extname = 'postgis_topology'
 ORDER BY sqladd;
 
 -- for postgis tiger geocoder
-SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || regexp_replace(
-    regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
+SELECT 'ALTER EXTENSION ' || extname || ' ADD ' || pg_catalog.regexp_replace(
+    pg_catalog.regexp_replace(pg_catalog.pg_describe_object(d.classid, d.objid, 0), E'cast from (.*) to (.*)', E'cast\(\\1 as \\2\)'),
     E'(.*) for access method (.*)', E'\\1 using \\2') || ';' AS sqladd
 FROM pg_catalog.pg_depend AS d
 INNER JOIN pg_extension AS e ON (d.refobjid = e.oid)
