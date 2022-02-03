0b39b0241270eb7941165d211d12027767dc0cbd
Sandro Santilli <strk@kbt.io>
2022-02-03 15:52:18 +0100

    Handle exception upon attempts to rewrite deprecated functions
    Closes #5080

--- utils/postgis_proc_upgrade.pl
+++ utils/postgis_proc_upgrade.pl
@@ -586,6 +586,8 @@ DO LANGUAGE 'plpgsql'
 \$postgis_proc_upgrade\$
 DECLARE
     deprecated_functions regprocedure[];
+    new_name TEXT;
+    rewrote_as_wrapper BOOLEAN;
     rec RECORD;
     extrec RECORD;
     procrec RECORD;
@@ -599,6 +601,8 @@ BEGIN
     FROM pg_catalog.pg_proc
     WHERE proname = ANY ('${deprecated_names}'::name[])
     INTO deprecated_functions;
+
+    RAISE DEBUG 'Handling deprecated functions: %', deprecated_functions;
 
 --    -- Rewrite views using deprecated functions
 --    -- to improve the odds of being able to drop them
@@ -641,7 +645,17 @@ BEGIN
     FOR rec IN SELECT unnest(deprecated_functions) as proc
     LOOP --{
 
+        RAISE DEBUG 'Handling deprecated function %', rec.proc;
+
+        new_name := regexp_replace(
+            rec.proc::text,
+            '_deprecated_by_postgis[^(]*\\(.*',
+            ''
+        );
+
         -- Rewrite the function as an SQL WRAPPER
+        -- TODO: don't even try if it's an aggregate or windowing
+        --       function
 
         SELECT pg_get_functiondef(oid) def, pronargs
         FROM pg_proc WHERE oid = rec.proc
@@ -660,11 +674,7 @@ BEGIN
             format(
                 -- TODO: have the function raise a warning too ?
                 'AS \$\$ SELECT %s(%s) \$\$',
-                regexp_replace(
-                    rec.proc::text,
-                    '_deprecated_by_postgis[^(]*\\(.*',
-                    ''
-                ),
+                new_name,
                 (
                     SELECT array_to_string(
                         array_agg('\$' || x),
@@ -676,7 +686,16 @@ BEGIN
         );
 
         RAISE DEBUG 'SQL: %', sql;
-        EXECUTE sql;
+        rewrote_as_wrapper := false;
+        BEGIN
+            EXECUTE sql;
+            rewrote_as_wrapper := true;
+        EXCEPTION
+        WHEN OTHERS THEN
+            RAISE WARNING
+                'Deprecated function % could not be rewritten as a wrapper: % (%)',
+                rec.proc, SQLERRM, SQLSTATE;
+        END;
 
         sql := format('DROP FUNCTION %s', rec.proc);
         --RAISE DEBUG 'SQL: %', sql;
@@ -690,11 +709,18 @@ BEGIN
                 hint = format(
                     'Replace the view changing all occurrences of %s in its definition with %s',
                     rec.proc,
-                    regexp_replace(rec.proc::text, '_deprecated_by_postgis[^(]*', '')
+                    new_name
                 );
             END IF;
             hint = hint || ' and upgrade again';
-            RAISE WARNING 'Deprecated function % left behind as a wrapper: %', rec.proc, SQLERRM
+            RAISE WARNING 'Deprecated function % left behind%: %',
+                rec.proc,
+                CASE
+                    WHEN rewrote_as_wrapper THEN
+                        ' as a wrapper'
+                    ELSE ''
+                END,
+                SQLERRM
             USING DETAIL = detail, HINT = hint;
 
             -- Drop the function from any extension it is part of
